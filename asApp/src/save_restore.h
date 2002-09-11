d58375e56821e4b0defe587e20d8685356242cec
bcda <prjemian@users.noreply.github.com>
2002-09-11 20:39:50 +0000

    *** empty log message ***

---
+++ asApp/src/save_restore.h
@@ -0,0 +1,20 @@
+/* save_restore.h */
+
+/*	
+ * 	Original Author: Frank Lenkszus
+ *	Current Author:  Frank Lenkszus
+ *	Date:		7/10/97
+ *
+ */
+
+#define         MAX(a,b)   ((a)>(b)?(a):(b))
+#define         MAXRESTOREFILES 8
+
+struct restoreList {
+        int pass0cnt;
+        int pass1cnt;
+        char *pass0files[MAXRESTOREFILES];
+        char *pass1files[MAXRESTOREFILES];
+};
+
+FILE *fopen_and_check(const char *file, const char *mode);
