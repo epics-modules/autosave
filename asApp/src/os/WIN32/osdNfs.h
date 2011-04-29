6d541083639f0318a77cdc10ef7fb479309d7e24
timmmooney <mooney@aps.anl.gov>
2011-04-29 22:23:39 +0000

    NFS_PATH_LEN increased to 255 mountFileSystem args deleted unused NFS error codes

--- asApp/src/os/WIN32/osdNfs.h
+++ asApp/src/os/WIN32/osdNfs.h
@@ -11,27 +11,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NFS_PATH_LEN 128                /* string length for NFS related path */
+#define NFS_PATH_LEN 255                /* string length for NFS related path */
 
 /* definition except for vxWorks */
 #define OK     0
 #define ERROR -1
 #define logMsg errlogPrintf
 
-/* NFS operation error codes */
-#define NFS_SUCCESS          0           /* succeed */
-#define NFS_FAILURE         -1           /* failure with unknown reasons */
-#define NFS_INVALID_HOST     1           /* uid_gid_host parameter is invalid */
-#define NFS_INVALID_PATH     2           /* path on the NFS server is invalid */
-#define NFS_INVALID_MNTPOINT 3           /* mount point in invalid */
-
-/* NFS operation definitions */
-#define UIDSEP            '@'
-
-/* routines for NFS operation */
-int nfsMount(char *uidhost, char *path, char *mntpoint);          /* mount the NFS (details) */
-
-int mountFileSystem(char *uidhost, char *path, char *mntpoint);   /* mount the NFS */
+int mountFileSystem(char *uidhost, char *addr, char *path, char *mntpoint);   /* mount the NFS */
 int dismountFileSystem(char *mntpoint);                           /* dismount the NFS */
 
 #endif
