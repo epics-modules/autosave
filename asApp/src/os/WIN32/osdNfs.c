c4949213a9df4108af8688164bbf6b0cb72e242d
timmmooney <mooney@aps.anl.gov>
2011-04-30 01:11:16 +0000

    mountFileSystem args, delete dummy nfsMount()

--- asApp/src/os/WIN32/osdNfs.c
+++ asApp/src/os/WIN32/osdNfs.c
@@ -12,13 +12,7 @@
 int save_restoreNFSOK    = 0;  /* Don't support NFS on this OS */
 int save_restoreIoErrors = 0;  /* for accumulate the IO error numbers, when the number larger than threshold, remount NFS */
 
-int nfsMount(char *uidhost, char *path, char *mntpoint)
-{
-    printf("nfsMount not supported on this OS\n");
-	return(0);  
-}
-
-int mountFileSystem(char *uidhost, char *path, char *mntpoint)
+int mountFileSystem(char *uidhost, char *addr, char *path, char *mntpoint)
 {
     printf("mountFileSytem not supported on this OS\n");
 	return(0);  
