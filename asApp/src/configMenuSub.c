7c24c340849ca28d8dab25c59443cfa289b8eb03
timmmooney <mooney@aps.anl.gov>
2013-03-20 18:58:13 +0000

    Rewrite: handle saves and restores, use callback to tell when the operation finishes and whether it succeeded

--- asApp/src/configMenuSub.c
+++ asApp/src/configMenuSub.c
@@ -1,28 +1,117 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <dbScan.h>
+#include <dbAccess.h>
 #include <aSubRecord.h>
 
-extern int fdbrestoreX(char *filename);
+typedef void (*callbackFunc)(int status, void *puserPvt);
+
+extern int fdbrestoreX(char *filename, char *macrostring, callbackFunc callbackFunction, void *puserPvt);
+extern char *getMacroString(char *request_file);
+
+extern int manual_save(char *request_file, int secsToWait,
+		char *save_file, callbackFunc callbackFunction, void *puserPvt);
 
 volatile int configMenuDebug=0;
+
+void configMenuCallback(int status, void *puserPvt) {
+	aSubRecord *pasub = (aSubRecord *)puserPvt;
+	long *d = (long *)pasub->d;
+
+	if (configMenuDebug)
+		printf("configMenuCallback:status=%d, puserPvt=%p\n", status, puserPvt);
+	dbScanLock((dbCommon *)pasub);
+	*d = (long)status;
+	dbScanUnlock((dbCommon *)pasub);
+	scanOnce((dbCommon *)puserPvt);
+}
 
 static long configMenu_init(aSubRecord *pasub) {
 	return(0);
 }
 
 /*
- * a - name of scan configuration to restore
+ * a - name of scan configuration to save or restore
  * b - return value from fdbrestore()
+ * c - current state of busy record
+ * d - status from callback (0 or -1)
+ * e - restore(0) or save(1)
+ * f - reqFile
+ * g - configMenu name
+ * vala - desired state of busy record (used to set)
+ * valb - desired state of busy record (used to clear)
+ * valc - status value for output: 0=Success, 1=Error
  */
 static long configMenu_do(aSubRecord *pasub) {
 	char *a = (char *)pasub->a;
-	int *b = (int *)pasub->b;
+	long *b = (long *)pasub->b;
+	char *c = (char *)pasub->c;
+	long *d = (long *)pasub->d;
+	short *e = (short *)pasub->e;
+	char *f = (char *)pasub->f;
+	char *g = (char *)pasub->g;
+	long *vala = (long *)pasub->vala;
+	long *valb = (long *)pasub->valb;
+	long *valc = (long *)pasub->valc;
+	char *macrostring = NULL;
+	char filename[100];
 
-	if (configMenuDebug) printf("configMenu_do:a='%s'\n", a);
-	if (strlen(a)<1) return(-1);
-	*b = fdbrestoreX(a);
-	if (configMenuDebug) printf("configMenu_do:fdbrestore returned %d\n", *b);
+	if (configMenuDebug) printf("configMenu_do:c='%s' (%s)\n",
+		c, *e?"save":"restore");
+
+	if (*e==0) {
+		/* restore */
+		if (strcmp(c,"Done") == 0) {
+			/* start restore operation */
+			if (configMenuDebug)
+				printf("configMenu_do:a='%s', c='%s', pasub=%p\n", a, c, pasub);
+			if (strlen(a)<1) {
+				*d = 1;
+				*valc = 1;
+				return(0);
+			}
+			if (f) {
+				macrostring = getMacroString(f);
+			}
+			sprintf(filename, "%s_%s.cfg", g, a);
+			*b = fdbrestoreX(filename, macrostring, configMenuCallback, (void *)pasub);
+			if (configMenuDebug) printf("configMenu_do:fdbrestore returned %ld\n", *b);
+			*vala = 1;
+			*valb = 1;
+		} else {
+			/* this is a callback from restore operation */
+			if (configMenuDebug)
+				printf("configMenu_do:callback status=%ld\n", *valc);
+			*valc = (*d ? 1 : 0);
+			*vala = 0;
+			*valb = 0;
+		}
+	} else {
+		/* save */
+		if (strcmp(c,"Done") == 0) {
+			/* start save operation */
+			if (configMenuDebug)
+				printf("configMenu_do:a='%s', c='%s', pasub=%p\n", a, c, pasub);
+			if (strlen(a)<1) {
+				*d = 1;
+				*valc = 1;
+				return(0);
+			}
+			sprintf(filename, "%s_%s.cfg", g, a);
+			*b = manual_save(f, 0, filename, configMenuCallback, (void *)pasub);
+			if (configMenuDebug) printf("configMenu_do:manual_save returned %ld\n", *b);
+			*vala = 1;
+			*valb = 1;
+		} else {
+			/* this is a callback from a save operation */
+			if (configMenuDebug)
+				printf("configMenu_do:save callback status=%ld\n", *valc);
+			*valc = (*d ? 1 : 0);
+			*vala = 0;
+			*valb = 0;
+		}
+	}
 	return(0);
 }
 
