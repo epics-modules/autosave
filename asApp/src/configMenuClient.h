bd97b00e770db48b71d2e3805dda863ea03b4c38
timmmooney <mooney@aps.anl.gov>
2013-04-01 15:48:06 +0000

    add findConfigFiles

--- asApp/src/configMenuClient.h
+++ asApp/src/configMenuClient.h
@@ -3,4 +3,5 @@ typedef void (*callbackFunc)(int status, void *puserPvt);
 extern int fdbrestoreX(char *filename, char *macrostring, callbackFunc callbackFunction, void *puserPvt);
 extern char *getMacroString(char *request_file);
 extern int manual_save(char *request_file, char *save_file, callbackFunc callbackFunction, void *puserPvt);
+extern int findConfigFiles(char *config, char names[][100], char descriptions[][100], int num, int len);
 
