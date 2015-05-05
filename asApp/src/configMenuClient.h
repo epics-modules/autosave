3ed1671e3b657b63421e35fde2cdfb26f9461dcb
timmmooney <mooney@aps.anl.gov>
2015-05-05 17:42:33 +0000

    parameterized findConfigFiles arg sizes

--- asApp/src/configMenuClient.h
+++ asApp/src/configMenuClient.h
@@ -1,7 +1,9 @@
 /* configMenuClient.h */
+#define CONFIGMENU_ITEM_CHARS 40
+
 typedef void (*callbackFunc)(int status, void *puserPvt);
 extern int fdbrestoreX(char *filename, char *macrostring, callbackFunc callbackFunction, void *puserPvt);
 extern char *getMacroString(char *request_file);
 extern int manual_save(char *request_file, char *save_file, callbackFunc callbackFunction, void *puserPvt);
-extern int findConfigFiles(char *config, char names[][100], char descriptions[][100], int num, int len);
+extern int findConfigFiles(char *config, char names[][CONFIGMENU_ITEM_CHARS], char descriptions[][CONFIGMENU_ITEM_CHARS], int num, int len);
 
