f9c7e0826e160fba3821a1abc60f1144168be177
timmmooney <mooney@aps.anl.gov>
2015-05-08 19:02:28 +0000

    Support unlimited number of config files, using list instead of arrays

--- asApp/src/configMenuClient.h
+++ asApp/src/configMenuClient.h
@@ -5,5 +5,10 @@ typedef void (*callbackFunc)(int status, void *puserPvt);
 extern int fdbrestoreX(char *filename, char *macrostring, callbackFunc callbackFunction, void *puserPvt);
 extern char *getMacroString(char *request_file);
 extern int manual_save(char *request_file, char *save_file, callbackFunc callbackFunction, void *puserPvt);
-extern int findConfigFiles(char *config, char names[][CONFIGMENU_ITEM_CHARS], char descriptions[][CONFIGMENU_ITEM_CHARS], int num, int len);
+extern int findConfigFiles(char *config, ELLLIST *configMenuList);
 
+struct configFileListItem {
+    ELLNODE node;
+	char *name;
+	char *description;
+};
