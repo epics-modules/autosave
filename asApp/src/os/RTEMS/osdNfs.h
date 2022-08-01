3acb1621d6b973f67493a75d7f9434bb4685e1ad
keenanlang <klang@aps.anl.gov>
2022-08-01 16:56:19 -0500

    patch addressing #45

--- asApp/src/os/RTEMS/osdNfs.h
+++ asApp/src/os/RTEMS/osdNfs.h
@@ -29,6 +29,13 @@
 #define NFS_INVALID_PATH     2           /* path on the NFS server is invalid */
 #define NFS_INVALID_MNTPOINT 3           /* mount point in invalid */
 
+#if __RTEMS_MAJOR__>4 || \
+    (__RTEMS_MAJOR__==4 && __RTEMS_MINOR__>9) || \
+    (__RTEMS_MAJOR__==4 && __RTEMS_MINOR__==9 && __RTEMS_REVISION__==99)
+/* Dropped from librtemsNfs.h in RTEMS 4.10, now defined in EPICS */
+int nfsMount(char *uidhost, char *path, char *mntpoint);
+#endif
+
 /* routines for NFS operation */
 int mountFileSystem(char *uidhost, char *addr, char *path, char *mntpoint);   /* mount the NFS */
 int dismountFileSystem(char *mntpoint);                           /* dismount the NFS */
