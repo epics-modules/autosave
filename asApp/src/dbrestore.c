10c992df3a74ed424c59c977dc6489e7ad70b746
Michael Davidsaver <mdavidsaver@gmail.com>
2025-07-21 14:21:23 -0500

    note origin of dbFindRecord failed

--- asApp/src/dbrestore.c
+++ asApp/src/dbrestore.c
@@ -954,7 +954,7 @@ int reboot_restore(char *filename, initHookState init_state)
 
             found_field = 1;
             if ((status = dbFindRecord(pdbentry, realName)) != 0) {
-                errlogPrintf("dbFindRecord for '%s' failed\n", PVname);
+                errlogPrintf("dbrestore:reboot_restore: dbFindRecord for '%s' failed\n", PVname);
                 num_errors++;
                 found_field = 0;
             } else if (dbFoundField(pdbentry) == 0) {
